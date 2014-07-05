b4dcfe0f775df0ca711bcfcc93f3fea1e01410aa
Sascha Brink <sascha.brink@gmail.com>
2014-07-05 14:14:51 +0200

    Fullscreen

--- app/scripts/controllers/main_ctrl.js
+++ app/scripts/controllers/main_ctrl.js
@@ -1,10 +1,5 @@
 angular.module('trivagoApp')
   .controller('mainCtrl', function($scope, $location) {
-    $scope.choices = [
-      [{ name: 'hot', value: '1' }, { name: 'cold', value: '2' }],
-      [{ name: 'left', value: '1' }, { name: 'right', value: '2' }],
-      [{ name: 'big', value: '1' }, { name: 'small', value: '2' }]
-    ]
 
     $scope.moveLeft = function(href) {
       $scope.direction = 'left';
