907df7f702efa1e768ea1a624a8ca8cb9e19d239
Tobias Block <gridsquare@gmail.com>
2014-07-05 17:38:11 +0200

    updating images and choices service

--- app/scripts/services/choices_service.js
+++ app/scripts/services/choices_service.js
@@ -2,18 +2,27 @@ angular.module('trivagoApp')
   .factory('choicesService', function() {
     var choices = [
       {
-        name: 'Do you like it... ?',
+        name: 'maindirection',
         items: [
-          { name: 'beach',    value: '1', image: '1-beach.jpg' },
           { name: 'city',     value: '2', image: '1-city.jpg' },
+          { name: 'beach',    value: '1', image: '1-beach.jpg' },
           { name: 'mountain', value: '3', image: '1-mountains.jpg' }
         ]
       },
       {
-        name: 'Which direction... ?',
+        name: 'maindirection',
         items: [
-          { name: 'left',  value: '1', image: '' },
-          { name: 'right', value: '2', image: '' }
+          { name: 'city',     value: '2', image: 'a1-city.jpg' },
+          { name: 'beach',    value: '1', image: 'a1-beach.jpg' },
+          { name: 'mountain', value: '3', image: 'a1-mountains.jpg' }
+        ]
+      },
+      {
+        name: 'art 	activity',
+        items: [
+          { name: 'party',  value: '1', image: '2-party.jpg' },
+          { name: 'silence', value: '2', image: '2-silence.jpg' },
+          { name: 'sport', value: '3', image: '2-sport.jpg' }
         ]
       },
       {
