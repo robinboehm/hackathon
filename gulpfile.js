b4539bd6f74b6126d4c117587dc55f9ee02fe0a5
Elmar Burke <elmar.burke@symetics.com>
2014-07-05 17:52:49 +0200

    Gulpfile mit browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 
-gulp.task('sass', function () {
-  gulp.src('./app/css/scss/*.scss')
-    .pipe(sass({ errLogToConsole: true }))
-    .pipe(gulp.dest('./app/css'));
+var paths = {
+  html: ['./app/*.html', './app/views/**/*.html'],
+  scripts: ['./app/scripts/**/*.js'],
+  sass: ['./app/css/scss/*.scss']
+};
+
+gulp.task('html', function() {
+  gulp.src(paths.html)
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['sass'], function () {
-  gulp.watch(['./app/css/scss/*.scss'], ['sass']);
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('sass', function () {
+  gulp.src(paths.sass)
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(reload({stream:true}));
+});
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'server'], function () {
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.sass, ['sass']);
 });
