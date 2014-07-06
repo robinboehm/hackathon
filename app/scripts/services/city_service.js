1461ad038b93bda817f60a63af5ca9b667a3a1bd
Sascha Brink <sascha.brink@gmail.com>
2014-07-06 14:08:59 +0200

    Fix city limit

--- app/scripts/services/city_service.js
+++ app/scripts/services/city_service.js
@@ -11,7 +11,7 @@ angular.module('trivagoApp')
       var result = cities.filter(function(city){
         return tagsArray.every(function(tag) { return city.tags.indexOf(tag) !== -1 });
       });
-      if (limit) { result.length = limit; }
+      if (limit && result.length > limit) { result.length = limit; }
       return result;
     }
 
