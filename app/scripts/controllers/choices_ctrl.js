7addbd69c65a78384a06b32b07b458af0cf98a5d
Elmar Burke <elmar.burke@symetics.com>
2014-07-06 14:07:14 +0200

    cities

--- app/scripts/controllers/choices_ctrl.js
+++ app/scripts/controllers/choices_ctrl.js
@@ -1,9 +1,11 @@
 angular.module('trivagoApp')
-  .controller('choicesCtrl', function($scope, choicesService) {
+  .controller('choicesCtrl', function($scope, choicesService, cityService) {
 
     $scope.choices = choicesService.choices;
     $scope.selectedChoices = choicesService.selectedChoices;
     $scope.choose = choicesService.choose;
+    $scope.tags = choicesService.tags;
+    $scope.findCities = cityService.getCitiesFromTags;
 
 
     $scope.$watch('choices.length', function(step, oldStep){
